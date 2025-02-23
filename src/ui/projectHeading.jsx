54ec845356facf4ed50691a217d7c3a7c9f3f91b
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2025-02-23 14:11:10 +0330

    edit responsive

--- src/ui/projectHeading.jsx
+++ src/ui/projectHeading.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import background from "../assets/images/833eb3f1ed14ecb9a6dfb717f9e3f826.jpg";
 
 const ProjectHeading = ({ title, date, subtitles, className = "" }) => {
+  const formattedDate = date ? new Date(date) : null;
+
+  const dayName = formattedDate
+    ? formattedDate.toLocaleDateString("fa-IR", { weekday: "long" }) // نام روز (شنبه، یکشنبه و ...)
+    : "";
+
+  const time = formattedDate
+    ? formattedDate.toLocaleTimeString("fa-IR", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }) // نمایش ساعت و دقیقه
+    : "";
+
   return (
     <div
-      className={` flex items-center justify-between p-8 text-center text-white bg-center bg-no-repeat bg-cover ${className}`}
+      className={`flex items-center justify-between p-8 text-center text-white bg-center bg-no-repeat bg-cover ${className}`}
       style={{
         backgroundImage: `url(${background})`,
       }}
     >
-      <div className="container flex justify-between ">
+      <div className="container flex justify-between">
         <div className="flex flex-row items-start w-full gap-4">
-          <h1 className="text-xl font-bold text-center m md:text-3xl ">
-            {title}
-          </h1>
-          <div className="flex flex-col text-sm text-right md:text-lg">
+          <h1 className="text-lg font-bold text-center ">{title}</h1>
+          <div className="flex flex-col text-sm text-right">
             {subtitles &&
               subtitles.map((subtitle, index) => (
                 <p key={index} className="mb-1 md:mb-2">
@@ -25,9 +36,11 @@ const ProjectHeading = ({ title, date, subtitles, className = "" }) => {
         </div>
 
         {date && (
-          <p className="mt-2 text-xs opacity-75 md:text-sm text-School-Bus">
-            {date}
-          </p>
+          <div className="flex flex-col gap-2 items-end mt-2 text-xs opacity-75 md:text-sm text-School-Bus">
+            <p>{dayName}</p> {/* نام روز */}
+            <p>{date}</p> {/* تاریخ اصلی */}
+            <p>{time}</p> {/* ساعت */}
+          </div>
         )}
       </div>
     </div>
